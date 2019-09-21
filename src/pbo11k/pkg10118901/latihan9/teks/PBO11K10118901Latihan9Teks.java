8928cec87159d8ab39d8f835dae50aa5a6851329
Gonior <43252085+Gonior@users.noreply.github.com>
2019-09-21 23:38:21 +0700

    Dibuat : Sabtu, 21 September 2019

---
+++ src/pbo11k/pkg10118901/latihan9/teks/PBO11K10118901Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA : Dedi Cahya 
+ * KELAS : IF-11-K 
+ * NIM : 10118901 
+ * DESKRIPSI : Mengenan lebih jauh tipe data char (karakter)
+ */
+public class PBO11K10118901Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'D';
+        System.out.println("Hasil " + newline + "char : "+tab + "berisi " +c);        
+    }
+    
+}
